06eb0aa6310cf7f41504fb74d04ce390d581219f
ElliotBrookes <elliotbrookes1@gmail.com>
2021-12-05 13:36:34 +0000

    centred and added outro animation function

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,9 @@
 <template>
   <section class="hero is-clipped is-fullheight">
     <div class="columns hero-body">
-      <div class="column is-two-fifths">
-        <p>Put image here</p>
-      </div>
       <div class="column">
-        <div class="has-text-centered">
-          <ul>
+        <div class="container">
+          <ul class="has-text-centered">
             <li>Elliot Brookes</li>
             <li>Born 30/05/2001</li>
             <li>Currently work as a software developer for Blockmark Technologies,
@@ -26,12 +23,35 @@
 </template>
 
 <script>
+import anime from 'animejs'
+
   export default {
     name:"About",
+    methods: {
+      outroSequence(){
+        return anime({
+          targets: '.has-text-centered',
+          translateY: '100%',
+          easing: "easeInExpo",
+        });
+      }
+    }
   }
 </script>
 
 <style scoped>
+section{
+  text-shadow: 2px 2px 2px grey;
+}
+.has-text-centered{
+  text-align: center;
+  max-width: 40%;
+  margin: 0 auto;
+}
+ul>li{
+  margin: 20px;
+  float: none;
+}
 .hero-body{
   background-color: #fffaf1;
 }
