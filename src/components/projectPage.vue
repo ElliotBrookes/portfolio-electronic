d44ced34ba24d9b522a25e062a8a7eddc0436595
ElliotBrookes <elliotbrookes1@gmail.com>
2021-12-13 20:42:49 +0000

    modal component used for viewing the projects

---
+++ src/components/projectPage.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+<div class="modal" :class='{"is-active" : isActive}'>
+  <div class="modal-background"></div>
+  <div class="modal-content is-clipped">
+      <p>Hello</p>
+      <div class="buttons">
+        <button class="button is-success">linkedin</button>
+        <button class="button">website</button>
+        <button class="button">github</button>
+      </div>
+  </div>
+  <button class="delete" aria-label="close" @click="$emit('close')"></button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "projectPage",
+  data () {
+    return {
+    }
+  },
+  props: {
+    isActive: Boolean,
+    content: Array,
+    links: Array,
+    titles: Array
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.modal-content{
+  background-color: white;
+  border-radius: 5px;
+  width: 90%;
+  height: 85%;
+}
+</style>
