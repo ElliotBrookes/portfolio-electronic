98a889771553c75ad8f28c2b1e5f7d87795c147e
ElliotBrookes <elliotbrookes1@gmail.com>
2021-12-13 20:42:36 +0000

    edits to how the animations take scaling, changed function args

--- src/components/base.vue
+++ src/components/base.vue
@@ -19,25 +19,25 @@
        </svg>
      </div>
      <div class="column">
-       <div class="body-text has-text-centered">
+       <div class="has-text-centered">
          <p class="title">Elliot Brookes' Portfolio</p>
-         <p>Software Developer based in the South West of England.</p>
+         <p class="body-text">Software Developer based in the South West of England.</p>
          <div class="">
            <p id="links">
              <ul class="level">
                <li class="level-item">
-                 <a href="https://www.linkedin.com/in/elliot-brookes-007550207/">
-                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                 <a  href="https://www.linkedin.com/in/elliot-brookes-007550207/" @mouseover="this.buttonEnlarge('#linkedin', 1.5)" @mouseleave="this.buttonShrink('#linkedin')">
+                   <svg id="linkedin" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
                  </a>
                </li>
                <li class="level-item">
-                 <a href="https://github.com/ElliotBrookes">
-                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                 <a  href="https://github.com/ElliotBrookes" @mouseover="this.buttonEnlarge('#github', 1.5)" @mouseleave="this.buttonShrink('#github')">
+                   <svg id="github" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
                  </a>
                </li>
                <li class="level-item">
-                 <a href="mailto:elliotbrookes1@gmail.com">
-                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
+                 <a href="mailto:elliotbrookes1@gmail.com" @mouseover="this.buttonEnlarge('#mail', 1.5)" @mouseleave="this.buttonShrink('#mail')">
+                   <svg id="mail" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
                  </a>
                </li>
              </ul>
@@ -51,9 +51,40 @@
 
 <script>
 import anime from 'animejs'
+// eslint-disable-next-line no-unused-vars
+import * as animations from '../assets/animations.js'
 
 export default {
   name: "Base",
+  methods: {
+    outroSequence(){
+      let outro = anime.timeline({
+        targets: '#heroGraphic',
+        translateY: '100%',
+        easing: "easeInExpo",
+        duration: 600
+      });
+
+      outro.add({
+        targets: '.body-text, .title, #mail, #github, #linkedin',
+        translateY: -300,
+        "color": '#fffaf1',
+        "fill": "#fffaf1",
+        'text-shadow': '0 0 0 #fffaf1',
+        "filter": "drop-shadow(0 0 0)",
+        easing: "easeInExpo",
+        duration: 600,
+        endDelay: 100
+      }, "+= 100")
+      return outro
+    },
+    buttonShrink(self){
+      return animations.buttonShrink(self)
+    },
+    buttonEnlarge(self, scale){
+      return animations.buttonEnlarge(self,scale)
+    }
+  },
   mounted() {
     anime({
       targets: '#heroGraphic',
@@ -61,15 +92,6 @@ export default {
       easing: "easeOutExpo"
     });
   },
-  methods: {
-    outroSequence(){
-      return anime({
-        targets: '#heroGraphic',
-        translateY: '100%',
-        easing: "easeInExpo",
-      });
-    }
-  }
 }
 </script>
 
@@ -88,7 +110,7 @@ section{
   bottom: -90%;
   left: 0px;
 }
-.body-text{
+.has-text-centered{
   margin-right: 50%;
 }
 #links{
